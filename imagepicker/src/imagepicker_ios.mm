6abe20f3715a5c10ffd57b337d125473d011f0d6
Chung Xa <wall85vn@yahoo.com>
2024-06-29 19:21:40 +0700

    Update for iOS

--- imagepicker/src/imagepicker_ios.mm
+++ imagepicker/src/imagepicker_ios.mm
@@ -2,8 +2,82 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <JQImagePicker.h>
 #include <dmsdk/sdk.h>
 #include "imagepicker_private.h"
+
+
+struct ImagePicker
+{
+	dmScript::LuaCallbackInfo*  m_Listener;
+};
+
+static ImagePicker g_ImagePicker;
+
+static char* CopyString(NSString* s)
+{
+	const char* osstring = [s UTF8String];
+	char* copy = strdup(osstring);
+	return copy;
+}
+
+
+@interface CImagePickerHandler : NSObject<JQImagePickerDelegate>
+
+UIViewController viewController;
+
+- (void)show;
+
+@end
+
+@implementation CImagePickerHandler
+
+- (void)show
+{
+	self.viewController = [[UIViewController alloc] init];
+
+	JQImagePicker *imagePicker = [JQImagePicker sharedInstance];
+	imagePicker.delegate = self;
+	[imagePicker showOriginalImagePickerWithType:ImagePickerPhoto InViewController:viewController];
+}
+
+#pragma mark - JQImagePicker Delegate
+
+- (void)imagePickerDidCancel:(JQImagePicker *)imagePicker
+{
+	NSLog(@"Cancelled");
+	[viewController release];
+}
+
+- (void)imagePicker:(JQImagePicker *)imagePicker didFinished:(UIImage *)editedImage
+{
+	// NSLog(@"%@",editedImage);
+	NSString* fileName = [[NSUUID UUID] UUIDString];
+	NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString* filePath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@fileName];
+	[UIImageJPEGRepresentation(editedImage, 1.0) writeToFile:filePath atomically:YES];
+	NSLog(@"%@",filePath);
+	[viewController release];
+
+	if (g_ImagePicker.m_Listener != 0)
+	{
+		lua_State* L = dmScript::GetCallbackLuaContext(g_ImagePicker.m_Listener);
+		DM_LUA_STACK_CHECK(L, 0);
+
+		if (dmScript::SetupCallback(g_ImagePicker.m_Listener))
+		{
+			lua_pushstring(L, CopyString(filePath));
+
+			dmScript::PCall(L, 2, 0);
+			dmScript::TeardownCallback(g_ImagePicker.m_Listener);
+			dmScript::DestroyCallback(g_ImagePicker.m_Listener);
+			g_ImagePicker.m_Listener = 0;
+		}
+	}
+}
+
+@end
+
 
 namespace ext_imagepicker {
 
@@ -13,6 +87,9 @@ void Initialize()
 
 void Show(dmScript::LuaCallbackInfo* callback)
 {
+	g_ImagePicker.m_Listener = callback;
+	CImagePickerHandler* imagePicker = [[CImagePickerHandler alloc] init];
+	[imagePicker show];
 }
 
 } // namespace
